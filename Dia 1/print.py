a2fc1e857e3457f1804fb91f39b1932f4d5f4a24
P4blitos <pablosernacardenas@gmail.com>
2023-09-18 14:37:55 -0500

    Se ve print y string

---
+++ Dia 1/print.py
@@ -0,0 +1,12 @@
+print("Esta es mi primerialinea python")
+
+#Actividad 1
+print("Me encanta estudiar Python")
+
+#Actividad 2
+#Crea un código que imprima en pantalla la expresión Estudiar con "Python Total" es super divertido
+print('Estudiar con "Python Total" es super divertido')
+
+#Actividad 3
+#Crea un código que imprima en pantalla el número 555, pero no debes imprimirlo directamente, sino como resultado de una operación matemática
+print(500+55)
---
+++ Dia 1/string.py
@@ -0,0 +1,5 @@
+print('100 + 50')
+print("Hola" + " Pablo")
+#alt + 92 es la barra invertida
+print("Hola \"Pablo\"")
+print82
