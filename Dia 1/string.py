f8ef7348e7048eafc7ae1484724043cd222bd384
P4blitos <pablosernacardenas@gmail.com>
2023-09-18 15:07:30 -0500

    Update string.py

--- Dia 1/string.py
+++ Dia 1/string.py
@@ -2,4 +2,15 @@ print('100 + 50')
 print("Hola" + " Pablo")
 #alt + 92 es la barra invertida
 print("Hola \"Pablo\"")
-print82
+print("Esta es una linea\nEsta tambien")#Saltar linea
+print("\tEres la mejor")#tabular 4 espacios
+print("This isn\'t un lenguage")
+
+#Actividad 1
+print("Línea 1\nLínea 2\nLínea 3")
+
+#Activivdad 2
+print("A\tB\tC\nD\tE\tF\nG\tH\tI")
+
+#Actividad 3
+print("Barra Normal: /\nBarra Invertida: \\")
