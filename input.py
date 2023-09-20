31d66cd0678858f3965ccc16e62cf88955eac910
P4blitos <pablosernacardenas@gmail.com>
2023-09-19 19:55:17 -0500

    Input
    Aprendi funciones con input

---
+++ input.py
@@ -0,0 +1,16 @@
+input("Dime tu nombre: ")
+
+print(input("Dime tu nombre: "))
+
+print("Tu nombre es: " + input("Dime tu nombre: "))
+
+print("Tu nombre es: " + input("Dime tu nombre: ")+ " "+"Tu apellido es: "+input("Dime tu apelido: "))
+
+#Actividad 1
+print(input("¿Qué estás estudiando?"))
+
+#Actividad 2
+print(input("¿En qué país vives?"))
+
+#Actvidad 3
+print(input("Escribe tu nombre:")+" "+input("Escribe tu apellido:"))
