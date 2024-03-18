6193deabb9d10516eab51256878702538a7bafbe
P4blitos <pablosernacardenas@gmail.com>
2024-03-18 18:07:28 -0500

    Variables en python

---
+++ Dia 2/Variables.py
@@ -0,0 +1,34 @@
+nombre="Pablo"
+print(nombre)
+
+nombre="Sonia"
+print(nombre)
+
+#numericos
+edad= 21
+edad2=50
+print(edad + edad2)
+
+#Pedir al usuario y guardarlo en una variable
+nombre2= input("Digita tu nombre: ")
+print("Tu nombre es: "+nombre2)
+
+palabra1= "Holaa "
+palabra2="Sir"
+print(palabra1+ palabra2)
+
+#ejercicio 1
+#Declara dos variables, llamadas nombre y edad. Asigna a la variable nombre el valor "Tony Soprano", y a la edad, el valor 51.
+nombre="Tony Soprano"
+edad=51
+
+#Ejercicio 2
+#Crea tres variables: nombre apellido nombrecompleto A nombre, asígnale el valor "Julia", y en apellido, asigna el valor "Roberts". Finalmente, construye la variable nombrecompleto concatenando las variables (recuerda sumar un espacio intermedio).
+nombre="Julia"
+apellido="Roberts"
+nombrecompleto=nombre+" "+apellido
+
+#Ejercicio 3
+
+curso="Python"
+print("Estás tomando un curso de "+curso)
