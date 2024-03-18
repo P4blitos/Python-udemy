89d4bd78747145824e8a64729ec6e58c40092769
P4blitos <pablosernacardenas@gmail.com>
2024-03-18 18:44:02 -0500

    enteros y flotantes

---
+++ Dia 2/Int_float.py
@@ -0,0 +1,38 @@
+my_number=1 + 3
+print(my_number)
+
+#Si quiero saber que tipo de variable es una es con type
+print(type(my_number))
+
+Second_number=7.5
+Second_number=Second_number+Second_number
+print(Second_number)
+print(type(Second_number))
+
+
+suma=5+5.8
+print(suma)
+print(type(suma))
+
+#cuando se ingresa por teclado
+edad=int(input("Ingresa tu edad"))
+print("tu edad es: ", edad)
+
+nueva_edad= edad+1
+print("Vas a cumplir: ", nueva_edad)
+
+
+#Ejercicio 1
+#Declara una variable numérica llamada num_entero que contenga un valor de tipo integer de tu elección. Imprime el tipo de dato de dicha variable.
+num_entero=20
+print(type(num_entero))
+
+#Ejercicio 2
+#Declara una variable numérica llamada num_decimal que contenga un valor de tipo float de tu elección. Imprime el tipo de dato de dicha variable.
+num_decimal=2.36
+print(type(num_decimal))
+
+#Ejercicio 3
+num1=7.5
+num2=2.5
+print(type(num1+num2))
