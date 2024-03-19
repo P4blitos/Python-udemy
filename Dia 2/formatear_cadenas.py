01443faaba384e3c7d7da6d6cd82d32df3205da3
P4blitos <pablosernacardenas@gmail.com>
2024-03-18 19:36:48 -0500

    Update formatear_cadenas.py

--- Dia 2/formatear_cadenas.py
+++ Dia 2/formatear_cadenas.py
@@ -11,3 +11,10 @@ matricula=54235
 print(f"El carro es {color} y su matricula es {matricula}")
 
 #Ejercicios 1
+#Necesitamos imprimir el nombre y número de asociado dentro de la siguiente frase: Estimado/a (nombre_asociado), su número de asociado es: (numero_asociado)
+
+nombre_asociado = "Jesse Pinkman"
+numero_asociado = 399058
+print(f"Estimado/a {nombre_asociado}, su número de asociado es: {numero_asociado}")
+
+#Ejercicio 2
