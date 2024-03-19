82d11c2ee4a1d10695903ca38d4c323a6719abac
P4blitos <pablosernacardenas@gmail.com>
2024-03-18 19:53:54 -0500

    Update formatear_cadenas.py

--- Dia 2/formatear_cadenas.py
+++ Dia 2/formatear_cadenas.py
@@ -18,3 +18,8 @@ numero_asociado = 399058
 print(f"Estimado/a {nombre_asociado}, su número de asociado es: {numero_asociado}")
 
 #Ejercicio 2
+puntos_nuevos = 350
+puntos_totales = 1225
+print(f"Has ganado {puntos_nuevos} puntos! En total, acumulas {puntos_totales} puntos")
+
+#Ejercicio 3 
