d3334cc6066c91f65454d15c03c097fbe6ea66e0
P4blitos <pablosernacardenas@gmail.com>
2024-03-18 19:28:20 -0500

    Create formatear_cadenas.py

---
+++ Dia 2/formatear_cadenas.py
@@ -0,0 +1,13 @@
+x=10
+y=5
+
+print("mis numeros de loteria son: {} y {}".format(x,y))
+print("La suma de: {} y {} es {}".format(x,y,x+y))
+
+#----------------------------------------------------
+color="Negro"
+matricula=54235
+
+print(f"El carro es {color} y su matricula es {matricula}")
+
+#Ejercicios 1
