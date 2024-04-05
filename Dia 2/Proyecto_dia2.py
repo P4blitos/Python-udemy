26074ab9a55995f688a1499a4b6ccd222ef39243
P4blitos <pablosernacardenas@gmail.com>
2024-04-04 22:00:12 -0500

    proyecto comisiones

---
+++ Dia 2/Proyecto_dia2.py
@@ -0,0 +1,6 @@
+print("Buenos dias, vengo a ayudarte a sacar la comision por las ventas del empleado")
+nombre=input("Primeramente, dame el nombre del empleado: ")
+total_ventas=float(input("Excelente!, ahora digita el precio total de todas las ventas"))
+total_ventas= total_ventas*0.13
+
+print(f"El empleado {nombre}, acaba ganar por comisiones un total de {total_ventas}, Felicidades!!!")
